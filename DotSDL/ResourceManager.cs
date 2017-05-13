faf2993d4b94742ca071ac2377e40a2112b4569b
Ian Burgmyer <spectere@gmail.com>
2017-05-13 09:28:59 -0400

    Began adding structures to track SDL objects with numeric IDs.

---
+++ DotSDL/ResourceManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DotSDL {
+    /// <summary>
+    /// This singleton keeps track of all the numbered SDL resources in the
+    /// system (window IDs, joystick IDs, etc). This is used to help dispatch
+    /// events.
+    /// </summary>
+    internal sealed class ResourceManager {
+        private static readonly Lazy<ResourceManager> Singleton = new Lazy<ResourceManager>(() => new ResourceManager());
+        internal static ResourceManager Instance => Singleton.Value;
+
+
+    }
+}
---
+++ DotSDL/ResourceType.cs
@@ -0,0 +1,11 @@
+﻿namespace DotSDL {
+    /// <summary>
+    /// Indicates a specific type of resource.
+    /// </summary>
+    internal enum ResourceType {
+        /// <summary>
+        /// An SDL_Window resource.
+        /// </summary>
+        Window
+    }
+}
