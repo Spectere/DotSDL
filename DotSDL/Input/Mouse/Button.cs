7abbaa05d16eef79af280fa9b1c21fc3389551d8
Ian Burgmyer <spectere@gmail.com>
2017-05-24 13:43:09 -0400

    Added all of the event structs, added several supporting enums for input events.

---
+++ DotSDL/Input/Mouse/Button.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DotSDL.Input.Mouse {
+    /// <summary>
+    /// The set of mouse buttons supported by SDL2.
+    /// </summary>
+    public enum Button : byte {
+        Left = 1,
+        Middle = 2,
+        Right = 3,
+        X1 = 4,
+        X2 = 5
+    }
+
+    /// <summary>
+    /// A mask used to indicate multiple buttons being pressed simultaneously.
+    /// </summary>
+    [Flags]
+    public enum ButtonMask {
+        Left = 0x0001,
+        Middle = 0x0002,
+        Right = 0x0004,
+        X1 = 0x0008,
+        X2 = 0x0010
+    }
+}
---
+++ DotSDL/Input/Mouse/WheelDirection.cs
@@ -0,0 +1,16 @@
+﻿namespace DotSDL.Input.Mouse {
+    /// <summary>
+    /// The direction of the mouse wheel.
+    /// </summary>
+    public enum WheelDirection : uint {
+        /// <summary>
+        /// The mouse wheel is normal (up scrolls up, down scrolls down).
+        /// </summary>
+        Normal,
+
+        /// <summary>
+        /// The mouse wheel is flipped (down scrolls up, up scrolls down).
+        /// </summary>
+        Flipped
+    }
+}
