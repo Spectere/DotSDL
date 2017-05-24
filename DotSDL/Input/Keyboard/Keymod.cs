d749b59fd62aaac085c6c5879bcddd933bb2a97c
Ian Burgmyer <iburgmyer@classicoptical.com>
2017-05-24 11:42:44 -0400

    Added list of key modifiers.

---
+++ DotSDL/Input/Keyboard/Keymod.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace DotSDL.Input.Keyboard {
+    [Flags]
+    public enum Keymod : ushort {
+        /// <summary>No keyboard modifiers.</summary>
+        None = 0x0000,
+
+        /// <summary>Left shift.</summary>
+        LShift = 0x0001,
+
+        /// <summary>Right shift.</summary>
+        RShift = 0x0002,
+
+        /// <summary>Left control.</summary>
+        LCtrl = 0x0040,
+
+        /// <summary>Right control.</summary>
+        RCtrl = 0x0080,
+
+        /// <summary>Left alt.</summary>
+        LAlt = 0x0100,
+
+        /// <summary>Right alt.</summary>
+        RAlt = 0x0200,
+
+        /// <summary>Left GUI key.</summary>
+        LGui = 0x0400,
+
+        /// <summary>Right GUI key.</summary>
+        RGui = 0x0800,
+
+        /// <summary>Num lock is on.</summary>
+        Num = 0x1000,
+
+        /// <summary>Caps lock is on.</summary>
+        Caps = 0x2000,
+        
+        Mode = 0x4000,
+
+        /// <summary>
+        /// Reserved.
+        /// </summary>
+        Reserved = 0x8000,
+
+        /// <summary>Either control key.</summary>
+        Ctrl = LCtrl | RCtrl,
+
+        /// <summary>Either shift key.</summary>
+        Shift = LShift | RShift,
+
+        /// <summary>Either alt key.</summary>
+        Alt = LAlt | RAlt,
+
+        /// <summary>Either GUI key.</summary>
+        Gui = LGui | RGui
+    }
+}
