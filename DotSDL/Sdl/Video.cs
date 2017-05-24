4c18af2196ad45cb40c90354141a4e93d6bc09d6
Ian Burgmyer <spectere@gmail.com>
2017-05-24 13:49:41 -0400

    Added window events.

--- DotSDL/Sdl/Video.cs
+++ DotSDL/Sdl/Video.cs
@@ -76,6 +76,62 @@ namespace DotSDL.Sdl {
         }
 
         /// <summary>
+        /// Events that can occur on a window.
+        /// </summary>
+        internal enum WindowEvent : byte {
+            /// <summary>Unused.</summary>
+            None,
+
+            /// <summary>Window has been shown.</summary>
+            Shown,
+
+            /// <summary>Window has been hidden.</summary>
+            Hidden,
+
+            /// <summary>Window has been exposed and should be redrawn.</summary>
+            Exposed,
+
+            /// <summary>Window has been moved.</summary>
+            Moved,
+
+            /// <summary>Window has been resized.</summary>
+            Resized,
+
+            /// <summary>The window size has changed, either as a result of an API call or through the system or user changing the window size.</summary>
+            SizeChanged,
+
+            /// <summary>Window has been minimized.</summary>
+            Minimized,
+
+            /// <summary>Window has been maximized.</summary>
+            Maximized,
+
+            /// <summary>Window has been restored to normal size and position.</summary>
+            Restored,
+
+            /// <summary>Window has gained mouse focus.</summary>
+            Enter,
+
+            /// <summary>Window has lost mouse focus.</summary>
+            Leave,
+
+            /// <summary>Window has gained keyboard focus.</summary>
+            FocusGained,
+
+            /// <summary>Window has lost keyboard focus.</summary>
+            FocusLost,
+
+            /// <summary>The window manager requests that the window be closed.</summary>
+            Close,
+
+            /// <summary>Window is being offered a focus (should SetWindowInputFocus() in itself).</summary>
+            TakeFocus,
+
+            /// <summary>Window had a hit test that wasn't SDL_HITTEST_NORMAL.</summary>
+            HitTest
+        }
+
+        /// <summary>
         /// Creates a window with the specified position, dimensions, and flags.
         /// </summary>
         /// <param name="title">The title of the window, in UTF-8 encoding.</param>
