94905e7e1032374ba4ff59ea1c1c724e31bd6467
Ian Burgmyer <iburgmyer@classicoptical.com>
2017-05-25 17:40:33 -0400

    Added .NET Core sample project.

---
+++ Samples/Sample.BasicPixelsCore/Line.cs
@@ -0,0 +1,18 @@
+﻿using DotSDL.Graphics;
+
+namespace Sample.BasicPixelsCore {
+    /// <summary>
+    /// Defines a line.
+    /// </summary>
+    public struct Line {
+        /// <summary>
+        /// The starting point of the <see cref="Line"/>.
+        /// </summary>
+        public Point Start;
+
+        /// <summary>
+        /// The ending point of the <see cref="Line"/>.
+        /// </summary>
+        public Point End;
+    }
+}
