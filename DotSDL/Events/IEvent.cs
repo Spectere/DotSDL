ce61ef74c32c7ab89dde3669e3e1d570efbf4e94
Ian Burgmyer <spectere@gmail.com>
2017-05-25 01:24:44 -0400

    Began implementing WindowEvents.

---
+++ DotSDL/Events/IEvent.cs
@@ -0,0 +1,12 @@
+﻿namespace DotSDL.Events {
+    /// <summary>
+    /// An interface describing a common DotSDL event.
+    /// </summary>
+    public interface IEvent {
+        /// <summary>
+        /// The timestamp of the event. Thsi is measured in milliseconds,
+        /// starting from when the application is first executed.
+        /// </summary>
+        uint Timestamp { get; set; }
+    }
+}
---
+++ DotSDL/Events/WindowEvent.cs
@@ -0,0 +1,11 @@
+﻿namespace DotSDL.Events {
+    /// <summary>
+    /// Represents a window event. Window events are thrown when an event occurs
+    /// that affects an <see cref="DotSDL.Graphics.SdlWindow"/> instance and/or
+    /// the applicaiton as a whole. This includes the UI toolkit minimizing,
+    /// maximizing, and closing a window, as well as application quit messages.
+    /// </summary>
+    public class WindowEvent : IEvent {
+        public uint Timestamp { get; set; }
+    }
+}
