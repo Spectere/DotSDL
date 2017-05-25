655162c0576c5f5053c822c3dd2ed0cd8b425b96
Ian Burgmyer <iburgmyer@classicoptical.com>
2017-05-25 15:48:39 -0400

    Began implementing event handling. Events are polled, and a method of converting event structs has been added.

--- DotSDL/Events/IEvent.cs
+++ DotSDL/Events/IEvent.cs
@@ -8,5 +8,11 @@
         /// starting from when the application is first executed.
         /// </summary>
         uint Timestamp { get; set; }
+
+        /// <summary>
+        /// The resources that this event should be dispatched to. If this
+        /// is null, the event will not be dispatched.
+        /// </summary>
+        IResourceObject Resource { get; set; }
     }
 }
