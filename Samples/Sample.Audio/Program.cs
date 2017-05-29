07ea201f803d9ec7bd9c44da99abb4022650019a
Ian Burgmyer <spectere@gmail.com>
2017-05-29 16:43:37 -0400

    Added sample project for audio support.

---
+++ Samples/Sample.Audio/Program.cs
@@ -0,0 +1,8 @@
+﻿namespace Sample.Audio {
+    class Program {
+        static void Main(string[] args) {
+            var window = new Window(512, 256);
+            window.Start(500, 16);
+        }
+    }
+}
---
+++ Samples/Sample.Audio/Window.cs
@@ -0,0 +1,9 @@
+﻿using DotSDL.Graphics;
+
+namespace Sample.Audio {
+    internal class Window : SdlWindow {
+        public Window(int width, int height) : base("DotSDL Audio Example", new Point{ X = WindowPosUndefined, Y = WindowPosUndefined }, width, height, 128, 32) {
+            
+        }
+    }
+}
