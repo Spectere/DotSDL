e57eb7b7c41c267986f508c3d9ad474347f8a507
Ian Burgmyer <spectere@gmail.com>
2017-05-29 22:59:49 -0400

    Added audio support. Currently only supports 16-bit little-endian.

---
+++ DotSDL/Audio/AudioFormat.cs
@@ -0,0 +1,26 @@
+﻿namespace DotSDL.Audio {
+    /// <summary>
+    /// The audio formats supported by DotSDL.
+    /// </summary>
+    public enum AudioFormat {
+        /// <summary>
+        /// An 8-bit integral audio format.
+        /// </summary>
+        Integer8,
+
+        /// <summary>
+        /// A 16-bit integral audio format.
+        /// </summary>
+        Integer16,
+
+        /// <summary>
+        /// A 32-bit integral audio format.
+        /// </summary>
+        Integer32,
+
+        /// <summary>
+        /// A 32-bit floating point audio format.
+        /// </summary>
+        Float32
+    }
+}
--- Samples/Sample.Audio/Window.cs
+++ Samples/Sample.Audio/Window.cs
@@ -1,5 +1,7 @@
-﻿using DotSDL.Graphics;
+﻿using DotSDL.Audio;
+using DotSDL.Graphics;
 using DotSDL.Input.Keyboard;
+using System;
 
 namespace Sample.Audio {
     internal class Window : SdlWindow {
@@ -17,7 +19,11 @@ namespace Sample.Audio {
 
         private const byte OnDelta = 24;
         private const byte OffColor = 72;
-        private Color TextColor = new Color { A = 255, R = 159, G = 159, B = 159 };
+        private readonly Color TextColor = new Color { A = 255, R = 159, G = 159, B = 159 };
+
+        private Playback _audio;
+        private ulong _time;
+        private int _audioFreq;
 
         public Window(int width, int height) : base("DotSDL Audio Example",
                                                     new Point{ X = WindowPosUndefined, Y = WindowPosUndefined },
@@ -25,6 +31,21 @@ namespace Sample.Audio {
                                                     36, 9) {
             KeyPressed += Window_KeyPressed;
             KeyReleased += Window_KeyReleased;
+
+            _audio = new Playback(44100, AudioFormat.Integer16, 1);
+            _audioFreq = _audio.Frequency;
+
+            Console.WriteLine($"Audio opened: {_audioFreq}hz, {_audio.BitSize}-bit, {_audio.Channels} channels, {_audio.BufferSizeSamples} sample buffer, {_audio.BufferSizeBytes} byte buffer");
+            _audio.BufferEmpty += Audio_BufferEmpty;
+
+            _maxFreq = _audioFreq / 2;
+            _audio.Play();
+        }
+
+        private void Audio_BufferEmpty(object sender, AudioBuffer e) {
+            var t = (Math.PI * 2.0 * _freq) / _audioFreq;
+            for(var i = 0; i < e.Samples.Length; i++)
+                e.Samples[i] = Math.Sin(_time++ * t);
         }
 
         private void DrawGlyph(ref Canvas canvas, char ch, int xPos, Color c) {
